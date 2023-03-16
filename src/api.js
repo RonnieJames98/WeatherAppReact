d570f767bb515e38827fd66fb2035192a2d8ac25
Ronnie James <ronniejespinosa1998@gmail.com>
2023-03-16 00:08:06 -0700

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a90f44590emsh800fa23639224d9p17b2a4jsn7b8b78d652bb',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "b315eed89cf66696bc2a6984bbba77f4";
